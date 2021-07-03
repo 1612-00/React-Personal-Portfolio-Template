0f37f9ff62fb4875773c3ed6bd55298e728fbb91
1612-00 <nguyenducanh.ldb@gmail.com>
2021-07-03 21:09:20 +0700

    Complete and reponsive for home page container

--- client/src/components/Tab.js
+++ client/src/components/Tab.js
@@ -1,14 +1,9 @@
-const Tab = ({icon, content}) => {
+const Tab = ({ icon, content }) => {
   return (
-    <div>
-      <li
-        className="react-tabs__tab"
-        id="react-tabs-0"
-      >
-        <img src={icon} alt="icon" className="svg" />
-        <span className="menu-content">{content}</span>
-      </li>
-    </div>
+    <li className="react-tabs__tab" id="react-tabs-0">
+      <img src={icon} alt="icon" className="svg" />
+      <span className="menu-content">{content}</span>
+    </li>
   );
 };
 
