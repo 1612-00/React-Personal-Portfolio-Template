7b3b2b55c94e0aeb8f8eed47834d48e080e17214
1612-00 <nguyenducanh.ldb@gmail.com>
2021-07-05 16:27:43 +0700

    Style for portfolio page

--- client/src/components/Portfolio/PortfolioContainer.js
+++ client/src/components/Portfolio/PortfolioContainer.js
@@ -3,7 +3,7 @@ import portfolio1 from "../../assets/portfolio-1.jpg";
 import portfolio2 from "../../assets/portfolio-2.jpg";
 import portfolio3 from "../../assets/portfolio-3.jpg";
 import portfolio4 from "../../assets/portfolio-4.jpg";
-import '../../css/Portfolio.css';
+import "../../css/Portfolio.css";
 
 const PortfolioContainer = () => {
   return (
@@ -28,54 +28,106 @@ const PortfolioContainer = () => {
             </TabList>
 
             <TabPanel>
-              <ul>
+              <ul className="portfolio-list">
                 <li>
-                  <a href="#">
-                    <img src={portfolio1} alt="portfolioImg" />
-                    <img src={portfolio2} alt="portfolioImg" />
-                    <img src={portfolio3} alt="portfolioImg" />
-                    <img src={portfolio4} alt="portfolioImg" />
-                  </a>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio1} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio2} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio3} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio4} alt="portfolioImg" />
+                    </a>
+                  </div>
                 </li>
               </ul>
             </TabPanel>
             <TabPanel>
-              <ul>
+              <ul className="portfolio-list">
                 <li>
-                  <a href="#">
-                    <img src={portfolio3} alt="portfolioImg" />
-                    <img src={portfolio4} alt="portfolioImg" />
-                  </a>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio3} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio4} alt="portfolioImg" />
+                    </a>
+                  </div>
                 </li>
               </ul>
             </TabPanel>
             <TabPanel>
-              <ul>
+              <ul className="portfolio-list">
                 <li>
-                  <a href="#">
-                    <img src={portfolio1} alt="portfolioImg" />
-                    <img src={portfolio2} alt="portfolioImg" />
-                  </a>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio1} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio2} alt="portfolioImg" />
+                    </a>
+                  </div>
                 </li>
               </ul>
             </TabPanel>
             <TabPanel>
-              <ul>
+              <ul className="portfolio-list">
                 <li>
-                  <a href="#">
-                    <img src={portfolio3} alt="portfolioImg" />
-                    <img src={portfolio4} alt="portfolioImg" />
-                  </a>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio3} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio4} alt="portfolioImg" />
+                    </a>
+                  </div>
                 </li>
               </ul>
             </TabPanel>
             <TabPanel>
-              <ul>
+              <ul className="portfolio-list">
                 <li>
-                  <a href="#">
-                    <img src={portfolio1} alt="portfolioImg" />
-                    <img src={portfolio2} alt="portfolioImg" />
-                  </a>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio1} alt="portfolioImg" />
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="inner">
+                    <a href="#">
+                      <img src={portfolio2} alt="portfolioImg" />
+                    </a>
+                  </div>
                 </li>
               </ul>
             </TabPanel>
